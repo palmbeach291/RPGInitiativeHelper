31372ac9c857a7bc3728a0f77ca2ee4422640c3a
Palmbeach2 <palmbeach291@gmail.com>
2024-03-25 18:45:33 +0100

    Initial

---
+++ RPGInitiativeHelper/Combat.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace RPGInitiativeHelper
+{
+    internal class Combat
+    {
+        public List<Fighter> Combatants { get; set; }
+        public int Turn { get; set; }
+        public int InitiavePhase { get; set; }
+        public Combat()
+        {
+            Combatants = new List<Fighter>();
+            Turn = 1;
+        }
+
+        public Combat(List<Fighter> combatants)
+        {
+            Combatants = combatants;
+            Turn = 1;
+        }
+
+        public int getStartInitiavePhase()
+        {
+            int retval = 0;
+
+            if (Combatants.Count > 0)
+            {
+                retval = Combatants.Max(fighter => fighter.Initiative); ;
+            }
+
+            return retval;
+        }
+
+        public void nextInitiavePhase()
+        {
+            int temp = 0;
+
+            foreach (Fighter f in Combatants)
+                if (f.Initiative < InitiavePhase && f.Initiative > temp)
+                    temp = f.Initiative;
+
+            if (temp > 0)
+                InitiavePhase = temp;
+            else
+            {
+                Turn++;
+                InitiavePhase = getStartInitiavePhase();
+            }
+        }
+    }
+}
---
+++ RPGInitiativeHelper/Fighter.cs
@@ -0,0 +1,81 @@
+﻿namespace RPGInitiativeHelper
+{
+    internal class Fighter
+    {
+        public string Name { set; get; }
+        public string Note { set; get; }
+        public int Initiative
+        {
+            set
+            {
+                Initiative += value;
+                if (Initiative < 0)
+                    Initiative = 0;
+            }
+            get
+            {
+                return Initiative;
+            }
+        }
+        public int Life
+        {
+            set
+            {
+                Life += value;
+                if (value < 0)
+                    Alive = false;
+                else if (Life > MaxLife)
+                    Life = MaxLife;
+            }
+            get
+            {
+                return Life;
+            }
+        }
+        public int MaxLife { set; get; }
+        public int Mana
+        {
+            set
+            {
+                Mana += value;
+                if (value < 0)
+                    Mana = 0;
+                else if (Mana > MaxMana)
+                    Mana = MaxMana;
+            }
+            get
+            {
+                return Mana;
+            }
+        }
+        public int MaxMana { set; get; }
+        public int Karma
+        {
+            set
+            {
+                Karma += value;
+                if (value < 0)
+                    Karma = 0;
+                else if (Karma > MaxKarma)
+                    Karma = MaxKarma;
+            }
+            get
+            {
+                return Karma;
+            }
+        }
+        public int MaxKarma { set; get; }
+        public bool Alive = true;
+
+        public Fighter(string name, int initiative, int maxLife, int maxMana, string note = "")
+        {
+            Name = name;
+            Initiative = initiative;
+            MaxLife = maxLife;
+            Life = maxLife;
+            MaxMana = maxMana;
+            Mana = maxMana;
+            Note = note;
+        }
+    }
+}
