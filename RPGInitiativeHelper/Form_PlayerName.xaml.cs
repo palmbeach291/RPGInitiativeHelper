cc196f078331ed90769d25ee630069da62370a0b
Palmbeach2 <palmbeach291@gmail.com>
2024-09-28 16:55:58 +0200

    Player Dialog improvment

--- RPGInitiativeHelper/Form_PlayerName.xaml.cs
+++ RPGInitiativeHelper/Form_PlayerName.xaml.cs
@@ -1,44 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace RPGInitiativeHelper
 {
-    /// <summary>
-    /// Interaktionslogik für Form_PlayerName.xaml
-    /// </summary>
     public partial class Form_PlayerName : Window
     {
         public string PlayerName { get; private set; }
+
         public Form_PlayerName()
         {
             InitializeComponent();
+
+            // Registriere das Loaded-Event des Fensters
+            this.Loaded += Form_PlayerName_Loaded;
+        }
+
+        // Event-Handler für das Loaded-Event
+        private void Form_PlayerName_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Setze den Fokus auf das TextBox-Element
+            TB_PlayerName.Focus();
         }
 
         private void OK_Click(object sender, RoutedEventArgs e)
         {
-            // Setze den Spielername auf den Wert des TextBoxes
             PlayerName = TB_PlayerName.Text;
-
-            // Schließe das Fenster
             this.Close();
         }
+
         private void Cancel_Click(object sender, RoutedEventArgs e)
         {
-            // Setze den Spielername auf null oder einen leeren String
             PlayerName = null;
-
-            // Schließe das Fenster
             this.Close();
         }
     }
